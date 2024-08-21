c0a6d8ab3198738d07a234c7731c50f0d10c23ba
bang-koon <dol7966@gmail.com>
2024-08-21 23:21:50 +0900

    feat(feature/#20): 호버 시 플롯 나오게 추가, 넘칠 경우 스크롤

--- app/_components/card.tsx
+++ app/_components/card.tsx
@@ -26,6 +26,9 @@ const Card = ({ movieDetail, hidden, rank }: CardProps) => {
             <span className={styles.cardRankBox}>
               <p className={styles.cardRank}>{rank ? rank : ""}</p>
             </span>
+            <div className={styles.overlay}>
+              <p className={styles.hiddenScroll}>{movieDetail.plot}</p>
+            </div>
           </div>
           <CardContents movieDetail={movieDetail} />
         </article>
