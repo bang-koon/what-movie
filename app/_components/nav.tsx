875b5c00e39a2f664fe438c6f0cc1cb5589d4db7
bang-koon <dol7966@gmail.com>
2024-12-09 19:11:56 +0900

    chore(feature/#27): 로고 추가 및 적용

--- app/_components/nav.tsx
+++ app/_components/nav.tsx
@@ -1,11 +1,11 @@
 import styles from "../main.module.scss";
-import Calendar from "./calendar";
 import Image from "next/image";
 
 const Header = () => (
-  <header className={styles.nav}>
-    <h1>영화 뭐 볼래?</h1>
-    {/* <Calendar /> */}
+  <header className={styles.header}>
+    <div className={styles.nav}>
+      <Image src="/logo.png" alt="logo" width={46} height={46} />
+    </div>
   </header>
 );
 
