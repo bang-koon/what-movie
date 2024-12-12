08687509de70cc23c3791765fb4bc8204c9d0894
bang-koon <dol7966@gmail.com>
2024-12-12 17:20:30 +0900

    feat(feature/#29): 700px보다 작으면 좌우 스크롤 UI로 변경

--- app/_components/main.tsx
+++ app/_components/main.tsx
@@ -4,8 +4,15 @@ import styles from "../main.module.scss";
 import { useState, useEffect } from "react";
 import { MainProps } from "../types";
 import Card from "./card";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useIsMobile } from "../hook/useIsMobile";
+import { Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const Main = ({ movieList }: MainProps) => {
+  const isMobile = useIsMobile();
   const [backgroundImage, setBackgroundImage] = useState<string | null>(null);
   const [hoveredCard, setHoveredCard] = useState<number | null | undefined>(
     null
@@ -43,20 +50,47 @@ const Main = ({ movieList }: MainProps) => {
         }}
       ></div>
       <main className={styles.mainContainer}>
-        <div className={styles.cardContainer}>
-          {movieList.map((v, i) => (
-            <Card
-              movieDetail={movieList[i]}
-              setBackgroundImage={setBackgroundImage}
-              hoveredCard={hoveredCard}
-              setHoveredCard={setHoveredCard}
-              key={i}
-              rank={i + 1}
-            />
-          ))}
-          <Card movieDetail={movieList[0]} empty={true}></Card>
-          <Card movieDetail={movieList[0]} empty={true}></Card>
-        </div>
+        {isMobile ? (
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            mousewheel={true}
+            loop={true}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+            className={styles.cardContainer}
+          >
+            {movieList.map((v, i) => (
+              <SwiperSlide key={i}>
+                <Card
+                  movieDetail={movieList[i]}
+                  setBackgroundImage={setBackgroundImage}
+                  hoveredCard={hoveredCard}
+                  setHoveredCard={setHoveredCard}
+                  rank={i + 1}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        ) : (
+          <div className={styles.cardContainer}>
+            {movieList.map((v, i) => (
+              <Card
+                movieDetail={movieList[i]}
+                setBackgroundImage={setBackgroundImage}
+                hoveredCard={hoveredCard}
+                setHoveredCard={setHoveredCard}
+                key={i}
+                rank={i + 1}
+              />
+            ))}
+            <Card movieDetail={movieList[0]} empty={true}></Card>
+            <Card movieDetail={movieList[0]} empty={true}></Card>
+          </div>
+        )}
       </main>
     </>
   );
